5cb3382911bb62de6572f15f2e86676a7fbffd21
Midehayzed <midehayzed@gmail.com>
2025-07-11 11:31:04 +0100

    basic operations

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print("The sum of", number1, "and", number2, "is:", sum) 
+difference = number1 - number2
+print("The difference between", number1, "and", number2, "is:", difference)
+product = number1 * number2
+print("The product of", number1, "and", number2, "is:", product)
