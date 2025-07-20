3912e4fe11b1a5a07d1c06ba486939d12d80484a
Midehayzed <midehayzed@gmail.com>
2025-07-20 07:38:07 +0100

    control flow operations

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -9,7 +9,11 @@ match operation:
     case "*":
         print(f"The result is: {num1 * num2}")
     case "/":
+        print(f"The result is: {num1 / num2}")
+    case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
             print(f"The result is: {num1 / num2}")
+    case _:
+        print("Invalid operation.")
